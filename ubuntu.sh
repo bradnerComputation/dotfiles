9bd4f3fbb6d9540317b6bcb3c2cd3ac701d3042f
Nick Semenkovich <semenko@alum.mit.edu>
2015-01-17 16:39:03 -0600

    WIP commit

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+COLOROFF='\033[0m'
+echo -e "${GREEN}hello, world${NOCOLOR}"
+
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
     echo "sudo $0 $*"
@@ -10,59 +14,44 @@ fi
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 
-apt-get update
 
-# Things I *hate*
-# apt-get purge 
+#### Sysctl tweaks
+echo "Updating sysctl" && sleep 3
 
-# Utilities
-apt-get install -y aptitude screen tmux htop iotop iftop
-
-# Things I like
-apt-get install -y git emacs24 mosh
-
-# Dev/building stuff
-apt-get install -y build-essential ccache gdb 
-
-# Python3
-apt-get install -y python3 pep8 pylint
-
-# Webdeb things
-apt-get install -y optipng pngcrush jpegoptim
-
-# Security-focused things
-apt-get install -y hardening-wrapper fail2ban nmap
-
-
-# Clean a bit
-apt-get autoremove
-apt-get autoclean
-
-
-#### Syscyl
 cp $SCRIPTPATH/conf/ubuntu/sysctl.d/*.conf /etc/sysctl.d/
 service procps restart
 
+
 #### Sudoers
 # Via: http://www.chromium.org/chromium-os/tips-and-tricks-for-chromium-os-developers
-cd /tmp
-cat > ./sudo_editor <<EOF
-EOF
-#!/bin/sh
-echo Defaults \!tty_tickets > \$1          # Entering your password in one shell affects all shells
-echo Defaults timestamp_timeout=120 >> \$1 # Time between re-requesting your password, in minutes
-EOF
-chmod +x ./sudo_editor
-sudo EDITOR=./sudo_editor visudo -f /etc/sudoers.d/relax_requirements
-EOF
+# TODO: Check this is actually included?
+echo "Relaxing sudoers" && sleep 3
+
+EDITOR=$SCRIPTPATH/conf/ubuntu/sudo_editor visudo -f /etc/sudoers.d/relax_requirements
+
+
+#### Grub text-only boot mode
+echo "Setting grub text-only and trim on dm-crypt" && sleep 3
+
+cp /etc/default/grub /etc/default/grub.old
+cp $CRIPTPATH/conf/ubuntu/grub /etc/default/grub
+update-grub
+
 
 #### Mount tweaks
+# echo "Setting mount tweaks" && sleep 3
 
 
 #### Generate ssh-key if missing
+echo "Generating ssh-key for device" && sleep 3
+ssh-keygen 
 
 
-### Grub text-only boot mode
+#### Hardening tweaks
+echo "Hardening sshd and ssh" && sleep 3
+
+
+
 
 
 #### Ubuntu UX/UI/Privacy tweaks
@@ -93,3 +82,41 @@ gsettings set com.canonical.Unity.ApplicationsLens display-available-apps false
 # No recently used
 gsettings set org.gnome.desktop.privacy remember-app-usage false
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+
+
+####
+# Install APT things
+####
+
+echo "Starting apt package installs, etc."
+sleep 3
+
+apt-get update
+
+# Things I *hate*
+# apt-get purge 
+
+# Utilities
+apt-get install -y aptitude screen tmux htop iotop iftop
+
+# Things I like
+apt-get install -y git emacs24 mosh
+
+# Dev/building stuff
+apt-get install -y build-essential ccache gdb 
+
+# Python3
+apt-get install -y python3 pep8 pylint
+
+# Webdeb things
+apt-get install -y optipng pngcrush jpegoptim
+
+# Security-focused things
+apt-get install -y hardening-wrapper fail2ban nmap
+
+
+# Clean a bit
+apt-get autoremove
+apt-get autoclean
+
