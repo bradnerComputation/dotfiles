3f6843c29493a4e0177ec0da236f511d2e2be4f6
Nick Semenkovich <semenko@alum.mit.edu>
2015-01-17 22:03:32 -0600

    Updated with gsettings, too.

--- ubuntu.sh
+++ ubuntu.sh
@@ -65,38 +65,9 @@ echo -e "${GREEN}Enabling auto updates?${NOCOLOR}" && sleep 3
 dpkg-reconfigure -plow unattended-upgrades
 
 
-exit
-
-
 #### Ubuntu UX/UI/Privacy tweaks
-# Derived from:
-# https://blogs.fsfe.org/the_unconventional/2015/01/14/improving-ubuntu-privacy/
-
-# Automount
-# gsettings set org.gnome.desktop.media-handling automount false
-# gsettings set org.gnome.desktop.media-handling automount-open false
-# gsettings set org.gnome.desktop.media-handling autorun-never true
-
-# Keyboard indicator
-gsettings set com.canonical.indicator.keyboard visible false
-
-# HUD
-gsettings set com.canonical.indicator.appmenu.hud store-usage-data false
-
-# Disable search scopes
-gsettings set com.canonical.Unity.Dash scopes "['applications.scope']"
-gsettings set com.canonical.Unity.Lenses always-search "['applications.scope']"
-gsettings set com.canonical.Unity.Lenses home-lens-default-view "['applications.scope']"
-gsettings set com.canonical.Unity.Lenses home-lens-priority "['applications.scope']"
-gsettings set com.canonical.Unity.Lenses remote-content-search none
-
-# Disable app suggestions
-gsettings set com.canonical.Unity.ApplicationsLens display-available-apps false
-
-# No recently used
-gsettings set org.gnome.desktop.privacy remember-app-usage false
-gsettings set org.gnome.desktop.privacy remember-recent-files false
-
+echo -e "${GREEN}Tweaking ubuntu UI settings as user...${NOCOLOR}" && sleep 3
+su ${REALUSER} -c "${SCRIPTPATH}/conf/ubuntu/gsettings-tweaks.sh"
 
 
 ####
