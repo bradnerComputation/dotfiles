7538d4643f6ae68aafdbc3cce1de6c7aabf04ba2
Nick Semenkovich <semenko@alum.mit.edu>
2015-01-19 09:46:28 -0600

    Fix gnubby, change timeouts.

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,19 +23,19 @@ cp $SCRIPTPATH/conf/ubuntu/sysctl.d/*.conf /etc/sysctl.d/
 service procps restart
 
 #### FIDO U2F Tokens
-echo -e "${GREEN}Udev rules for FIDO${NOCOLOR"} && sleep 2
+echo -e "${GREEN}Udev rules for FIDO${NOCOLOR}" && sleep 2
 cp $SCRIPTPATH/conf/ubuntu/50-fido.rules /etc/udev/rules.d/
 						
 #### Sudoers
 # Via: http://www.chromium.org/chromium-os/tips-and-tricks-for-chromium-os-developers
 # TODO: Check this is actually included?
-echo -e "${GREEN}Relaxing sudoers${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Relaxing sudoers${NOCOLOR}" && sleep 2
 
 EDITOR=$SCRIPTPATH/conf/ubuntu/sudo_editor visudo -f /etc/sudoers.d/relax_requirements
 
 
 #### Grub text-only boot mode
-echo -e "${GREEN}Setting grub text-only and trim on dm-crypt${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Setting grub text-only and trim on dm-crypt${NOCOLOR}" && sleep 2
 
 cp /etc/default/grub /etc/default/grub.old
 cp $SCRIPTPATH/conf/ubuntu/grub /etc/default/grub
@@ -43,11 +43,11 @@ update-grub
 
 
 #### Mount tweaks
-# echo "Setting mount tweaks" && sleep 3
+# echo "Setting mount tweaks" && sleep 2
 
 
 #### Generate ssh-key if missing
-echo -e "${GREEN}Generating ssh-key for device${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Generating ssh-key for device${NOCOLOR}" && sleep 2
 if [ ! -f "/home/${REALUSER}/.ssh/id_rsa" ]; then
     su ${REALUSER} -c 'ssh-keygen -b 4096 -C "$(whoami)@$(hostname)-$(date -I)"'
 else
@@ -56,20 +56,20 @@ fi
 
 
 #### SSH tweaks
-echo -e "${GREEN}Hardening sshd and ssh${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Hardening sshd and ssh${NOCOLOR}" && sleep 2
 
 
 #### Ramdisk creation
-echo -e "${GREEN}Ramdisk creation${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Ramdisk creation${NOCOLOR}" && sleep 2
 
 
 ### Auto updates
-echo -e "${GREEN}Enabling auto updates?${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Enabling auto updates?${NOCOLOR}" && sleep 2
 dpkg-reconfigure -plow unattended-upgrades
 
 
 #### Ubuntu UX/UI/Privacy tweaks
-echo -e "${GREEN}Tweaking ubuntu UI settings as user...${NOCOLOR}" && sleep 3
+echo -e "${GREEN}Tweaking ubuntu UI settings as user...${NOCOLOR}" && sleep 2
 su ${REALUSER} -c "${SCRIPTPATH}/conf/ubuntu/gsettings-tweaks.sh"
 
 
