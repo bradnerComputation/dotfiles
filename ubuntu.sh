435ef7eab11b31950c4c4dadda475925c7246413
Nick Semenkovich <semenko@alum.mit.edu>
2015-05-23 17:12:50 -0500

    +chrome

--- ubuntu.sh
+++ ubuntu.sh
@@ -115,6 +115,11 @@ apt-get install -y \
 apt-get autoremove
 apt-get autoclean
 
+# Get Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get install google-chrome-stable google-chrome-beta google-chrome-unstable
+
 
 ## Configure netplug
 echo -e "${GREEN}Netplug config${NOCOLOR}" && sleep 1
